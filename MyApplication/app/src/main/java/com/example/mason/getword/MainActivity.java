dc214aa89c1e081f4c3cbf97e73b8afe99f1c0bc
masonlikes <lawrencemason00@gmail.com>
2017-05-16 18:23:15 -0600

    Text changes on button click

--- MyApplication/app/src/main/java/com/example/mason/getword/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/mason/getword/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.mason.getword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                //change the top text
+                changeText();
+            }
+
+        });
+    }
+
+    private void changeText(){
+        setContentView(R.layout.activity_main);
+        final TextView textViewToChange = (TextView) findViewById(R.id.textView);
+        textViewToChange.setText("change");
     }
 }
