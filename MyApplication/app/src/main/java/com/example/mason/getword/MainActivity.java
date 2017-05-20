3a9ec8f27bd700fa970bcf7caa39458fd3a4c4c6
masonlikes <lawrencemason00@gmail.com>
2017-05-19 20:32:06 -0600

    Now they only delete if they are not in word

--- MyApplication/app/src/main/java/com/example/mason/getword/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/mason/getword/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +34,32 @@ public class MainActivity extends AppCompatActivity {
         container.addView(selfDestructBtn);
 
 
+    }
+
+    private void changeText(){
+        final TextView textViewToChange = (TextView) findViewById(R.id.word_text);
+        textViewToChange.setText(returnRandomWord());
+    }
+
+    private String returnRandomWord(){
+        String randomWord = "";
+        try {
+
+            InputStream is = getAssets().open("Words.txt");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            String fileContent = new String(buffer, "UTF-8");
+            String[] contentArray = fileContent.split(",");
+            randomWord = contentArray[rand.nextInt(contentArray.length)];
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return randomWord;
     }
 }
