0a8c9323f534ca8d7b39799b189f1e66acb46e5d
Thomas Feuerborn <tfeuerbo@gmail.com>
2017-06-01 16:16:25 -0600

    difficulty working

--- MyApplication/app/src/main/java/com/example/mason/getword/Hangman.java
+++ MyApplication/app/src/main/java/com/example/mason/getword/Hangman.java
@@ -3,22 +3,34 @@ package com.example.mason.getword;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
+import com.example.mason.getword.WordDifficulty;
 
 public class Hangman extends AppCompatActivity {
 
     Random rand = new Random();
+
+    final static int EASY = 150, MEDIUM = 300, HARD = 450;
+
+    List<String> EasyWords = new ArrayList<>();
+    List<String> MediumWords = new ArrayList<>();
+    List<String> HardWords = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
 
+        loadWords();
 
         /* Check if they want to play again */
         if(getIntent().getBooleanExtra("PlayAgain", false)){
@@ -36,22 +48,82 @@ public class Hangman extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                i.putExtra("WORD", getRandomWord());
-                i.putExtra("Difficulty", 1);
+                i.putExtra("WORD", getRandomWord(getIntent().getIntExtra("Difficulty", -1)));
+                i.putExtra("Difficulty", -1);
                 startActivity(i);
             }
         });
 
 
         Button easy = (Button) findViewById(R.id.easy_game);
+        easy.setOnClickListener(new DifficultyButtonClickListener(1));
         Button medium = (Button) findViewById(R.id.medium_game);
+        medium.setOnClickListener(new DifficultyButtonClickListener(2));
         Button hard = (Button) findViewById(R.id.hard_game);
+        hard.setOnClickListener(new DifficultyButtonClickListener(3));
 
         //add click listeners here
     }
 
+    private class DifficultyButtonClickListener implements Button.OnClickListener{
+        private final int d;
+        public DifficultyButtonClickListener(int d){
+            this.d = d;
+        }
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            i.putExtra("WORD", getRandomWord(d));
+            i.putExtra("Difficulty", d);
+            startActivity(i);
+        }
+    }
+
     private String getRandomWord(int difficulty){
-        return "";
+        switch (difficulty){
+            case 1:
+                return EasyWords.get(rand.nextInt(EasyWords.size()));
+            case 2:
+                return MediumWords.get(rand.nextInt(MediumWords.size()));
+            case 3:
+                return HardWords.get(rand.nextInt(HardWords.size()));
+            default:
+                return getRandomWord();
+
+        }
+    }
+
+    private void loadWords(){
+        try{
+            InputStream is = getAssets().open("Words.txt");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            String fileContent = new String(buffer, "UTF-8");
+            String[] contentArray = fileContent.split(",");
+
+            for (String word: contentArray) {
+                double diff = WordDifficulty.getWordDiff(word);
+                Log.i("Word", word+"");
+                Log.i("Diff", diff+"");
+                if(diff < EASY){
+                    EasyWords.add(word);
+                } else if(diff >= EASY && diff < MEDIUM ) {
+                    MediumWords.add(word);
+                } else if(diff >= MEDIUM){
+                    HardWords.add(word);
+                } else {
+                    throw new IllegalStateException();
+                }
+            }
+            Log.i("Easy", EasyWords.size()+"");
+            Log.i("Med", MediumWords.size()+"");
+            Log.i("Hard", HardWords.size()+"");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private String getRandomWord(){
